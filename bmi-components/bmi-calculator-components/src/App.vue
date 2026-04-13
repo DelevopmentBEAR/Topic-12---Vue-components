0ca2d295ce938e2b6333a79f05101c45f7046581
Berit Haugen <movies.tv.and.music@gmail.com>
2026-04-12 19:06:35 -0500

    first commit

---
+++ bmi-components/bmi-calculator-components/src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import BodyMassIndexForm from './components/BodyMassIndexForm.vue'
+
+import { ref } from 'vue'
+
+const bmi = ref() // value is undefined which is "Falsey"
+
+function calculateBmi(height, weight) {
+  // BMI formula: weight (kg) / (height (m) * height (m))
+  // alert('Calculating BMI...' + ' ' + height + ' ' + weight)
+   bmi.value = weight / (height * height)
+}
+
+</script>
+
+<template>
+  <h1>Body Mass Index Calculator</h1>
+
+  <BodyMassIndexForm v-on:stats-entered="calculateBmi" />
+
+<h3>Results</h3>
+  <p v-if="bmi">Your BMI is: {{ bmi.toFixed(2) }}</p>
+  <p v-else>Please enter data into the form.</p>
+
+  <!-- App.vues knows about BodyMassIndexForm -->
+  <!-- BodyMassIndexForm does NOT know about App.vue -->
+  <!-- That's by design, so we can re-use components -->
+</template>
---
+++ bmi-components/bmi-calculator-components/src/components/BodyMassIndexForm.vue
@@ -0,0 +1,36 @@
+<script setup>
+
+import { ref, defineEmits, h } from 'vue'
+
+const emit = defineEmits(['stats-entered'])
+
+const height = ref(0)
+const weight = ref(0)
+
+
+function statsEntered() {
+  // need to emit an event with the calculated BMI value
+  // the event will be received by the parent component (App.vue) and displayed there
+  // this component does not know and does not care about how the BMI value is displayed, it just emits the value to the parent component
+  emit('stats-entered', height.value, weight.value)
+}
+
+</script> 
+
+<template> 
+
+<h2>Enter your Height and Weight</h2>
+
+<label for="height">Height in meters:</label>
+<input type="number" id="height" v-model="height" />
+
+<label for="weight">Weight in kilograms:</label>
+<input type="number" id="weight" v-model="weight" />
+
+<button type="submit" v-on:click="statsEntered">Calculate BMI</button>
+
+</template>
+
+<style scoped> 
+
+</style>
