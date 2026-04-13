b3b727ad815bde3d1144fa20c74f66871ccb2896
Berit Haugen <movies.tv.and.music@gmail.com>
2026-04-12 20:45:16 -0500

    Vue Components WYR Component, Props, Radio Buttons

--- would-you-rather/src/App.vue
+++ would-you-rather/src/App.vue
@@ -1,7 +1,23 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import WouldYouRather from './components/WouldYouRather.vue'
+import {ref} from 'vue'
+
+// These will hold the question and answers for the "Would You Rather" game
+const wyrQuestion = ref('Would you rather be invisible or be able to fly?')
+const wyrAnswer1 = ref('Be invisible')
+const wyrAnswer2 = ref('Be able to fly')
+
+// This will store the user's choice
+const userChoice = ref('')
+
 </script>
 
 <template>
-  <HelloWorld />
+  <h1>Would You Rather?</h1>
+
+  <!-- Passes the question and answers as props to the WouldYouRather component -->
+  <WouldYouRather 
+  v-bind:question="wyrQuestion" 
+  v-bind:answer1="wyrAnswer1" 
+  v-bind:answer2="wyrAnswer2" />
 </template>
---
+++ would-you-rather/src/components/WouldYouRather.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from 'vue';
+
+defineProps({
+  // These props will receive the question and answers from the parent component (App.vue)
+  question: String,
+  answer1: String,
+  answer2: String
+})
+
+const choice = ref('') // This will store the user's choice
+
+</script>
+
+<template>
+  <h2>Make your choice!</h2>
+
+  <p>{{ question }}</p>
+  <div class="radio-group">
+    <!-- v-model binds the input to the choice ref -->
+    <!-- v-bind connects the input to the corresponding answer prop -->
+    <!-- id binds the label to the input -->
+    <input v-model="choice" v-bind="answer1" type="radio" id="answer1">
+    <label for="answer1">{{ answer1 }}</label>
+
+    <input v-model="choice" v-bind="answer2" type="radio" id="answer2">
+    <label for="answer2">{{ answer2 }}</label>
+  </div>
+</template>
+
+<style scoped>
+/* I wanted to keep the radio buttons inline */
+.radio-group {
+  display: inline; 
+  gap: 10px; 
+}
+</style>
